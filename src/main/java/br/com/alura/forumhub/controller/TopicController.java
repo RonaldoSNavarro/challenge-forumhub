98e739441b0f640e016e818500abb4a9e1927d8d
ronaldosnavarro <navarro.silva@terra.com.br>
2024-07-10 21:44:02 -0300

    Criando controlador

---
+++ src/main/java/br/com/alura/forumhub/controller/TopicController.java
@@ -0,0 +1,46 @@
+package br.com.alura.forumhub.controller;
+
+import br.com.alura.forumhub.model.Topic;
+import br.com.alura.forumhub.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/topics")
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @GetMapping
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Topic> getTopicById(@PathVariable Long id) {
+        return topicService.getTopicById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Topic createTopic(@Valid @RequestBody Topic topic) {
+        return topicService.createTopic(topic);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Topic> updateTopic(@PathVariable Long id, @Valid @RequestBody Topic topic) {
+        return ResponseEntity.ok(topicService.updateTopic(id, topic));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTopic(@PathVariable Long id) {
+        topicService.deleteTopic(id);
+        return ResponseEntity.noContent().build();
+    }
+}
